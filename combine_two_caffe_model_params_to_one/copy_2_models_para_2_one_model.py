9821e2e258712fadd26f3d94a5c6bd52f28a9e6b
Porridge Zhou <jiqiangzhou@foxmail.com>
2018-02-06 10:31:31 +0800

    Update copy_2_models_para_2_one_model.py

--- combine_two_caffe_model_params_to_one/copy_2_models_para_2_one_model.py
+++ combine_two_caffe_model_params_to_one/copy_2_models_para_2_one_model.py
@@ -10,14 +10,14 @@ sys.path.append(CAFFE_ROOT_TSN+'python')
 import caffe
 caffe.set_device(0)
 
-SRC_NET_1 = "/home/users/zhoujiqiang/TSN/flownet2/FlowNet2-S/train.prototxt"
-SRC_WEIGHTS_1 = "/home/users/zhoujiqiang/TSN/flownet2/FlowNet2-S/FlowNet2-S_weights.caffemodel.h5"
+SRC_NET_1 = "train.prototxt"
+SRC_WEIGHTS_1 = "weights.caffemodel.h5"
 
-SRC_NET_2 = "./models/ucf101/tsn_bn_inception_flow_train_val.prototxt"
-SRC_WEIGHTS_2 = "./models/ucf101_split1_tsn_bn_inception_Flownet_iter_18000.caffemodel"
+SRC_NET_2 = "_train_val.prototxt"
+SRC_WEIGHTS_2 = "18000.caffemodel"
 
-TGT_NET = './models/ucf101/tsn_flownet2_bn_inception_flow_frozenTSN_norm_flownet_output_train_val.prototxt'
-TGT_NET_MODEL = './models/FlowNetxTSN_TSN_with_flowNet_img_18000_weights.caffemodel'
+TGT_NET = 'output_train_val.prototxt'
+TGT_NET_MODEL = '18000_weights.caffemodel'
 
 net_1 = caffe.Net(SRC_NET_1, SRC_WEIGHTS_1, caffe.TRAIN)
 net_2 = caffe.Net(SRC_NET_2, SRC_WEIGHTS_2, caffe.TRAIN)
